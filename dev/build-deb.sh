f66337c31d14f0e42a527d6fafd839f21fb5707c
AZ Huang <aitjcize@gmail.com>
2010-07-05 23:09:12 +0800

    Add dev directory

---
+++ dev/build-deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 'Version: '
+read ver
+
+./chver.sh $ver
+cd ../../
+cp -r manpages-cpp manpages-cpp-$ver
+mv manpages-cpp-$ver manpages-cpp/dev
+cd manpages-cpp/dev/
+tar -zcf manpages-cpp-$ver.orig.tar.gz manpages-cpp-$ver
+
+cd manpages-cpp-$ver
+dh_make -s -b
+
+echo `pwd`
+cp dev/rules dev/control debian
+cd debian
+rm *.ex *.EX README.*
+cd ..
+fakeroot debian/rules binary
---
+++ dev/chver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -i "s/program_version = '.*'/program_version = '$1'/" ../bin/cppman
+sed -i "s/version = '.*'/version = '$1'/" ../setup.py
