28da6defe78e48e583f61680924510947235a652
AZ Huang <aitjcize@gmail.com>
2010-07-05 14:18:12 +0800

    Improve formatter.

--- cppman/Formatter.py
+++ cppman/Formatter.py
@@ -32,21 +32,22 @@ import termios
 import urllib
 
 # Format replacement RE list
+# The '.SE' pseudo macro is described in function: cplusplus2groff
 rps = [
         # Header, Name
-        (r'<div class="doctop"><h1>(.+?)</h1><div class="right"><br>(.+?)</div>'
-         r'</div><div class="docsubtop"><div class="right">(.*)</div>\n'
-         r'<div class="prototype">(.*)</div>\n</div>'
+        (r'<div class="doctop"><h1>(.+?)</h1><div class="right"><br>(.*?)</div>'
+         r'</div><div class="docsubtop"><div class="right"><code>(.*)</code>'
+         r'</div>\n<div class="prototype">(.*)</div>\n</div>'
          r'<p><strong>(.+?)</strong></p>\n</div><div id="content">(.+?)<br>',
          r'.TH "\1" 3 "%s" "cplusplus.com" "C++ Programmer\'s Manual"\n'
          r'\n.SH NAME\n\1 - \5\n'
-         r'\n.SH TYPE\n\2\n'
-         r'\n.SH SYNOPSIS\n\4\n'
-         r'\n.SH DESCRIPTION\n\6\n.sp\n' % datetime.date.today(), re.DOTALL),
+         r'\n.SE\n.SH TYPE\n\2\n'
+         r'\n.SE\n.SH SYNOPSIS\n#include \3\n.sp\n\4\n'
+         r'\n.SE\n.SH DESCRIPTION\n\6\n.sp\n'% datetime.date.today(),re.DOTALL),
         # Remove empty sections
         (r'\n.SH (.+?)\n\n', r'', 0),
         # Section headers
-        (r'.*<h3>(.+?)</h3>', r'.SH "\1"\n', 0),
+        (r'.*<h3>(.+?)</h3>', r'\n.SE\n.SH "\1"\n', 0),
         # 'ul' tag
         (r'<ul>', r'\n.in +2n\n.sp\n', 0),
         (r'</ul>', r'\n.in\n', 0),
@@ -86,7 +87,8 @@ rps = [
         
         # Footer
         (r'<div id="footer">.*?</div>',
-         r'\n.SH REFERENCE\ncplusplus.com, 2000-2010 - All rights reserved.',
+         r'\n.SE\n.SH REFERENCE\n'
+         r'cplusplus.com, 2000-2010 - All rights reserved.',
          re.DOTALL),
         # 'br' tag
         (r'<br>', r'\n.br\n', 0),
@@ -127,8 +129,7 @@ def cplusplus2groff(data):
     code_sections = re.findall(r'<code>.+?</code>', data, re.DOTALL)
     for st in code_sections:
         sts = re.sub(r'\n', r'\n.br\n', st)
-        index = data.index(st)
-        data = data[:index] + sts + data[index + len(st):]
+        data = data.replace(st, sts)
 
     # Replace all
     for rp in rps:
@@ -136,55 +137,43 @@ def cplusplus2groff(data):
 
     # Upper case all section headers
     for st in re.findall(r'.SH .*\n', data):
-        index = data.index(st)
-        data = data[:index] + st.upper() + data[index + len(st):]
+        data = data.replace(st, st.upper())
 
     # Add tags to member functions
-    if 'class' in re.search(r'\n\.SH TYPE\n(.+?)\n', data).group(1):
+    # e.g. insert -> vector::insert
+    #
+    # .SE is a pseudo macro I created which means 'SECTION END'
+    # The reason I use it is because I need a marker to know where section ends.
+    # re.findall find patterns which do not overlap, which means if I do this:
+    # secs = re.findall(r'\n\.SH "(.+?)"(.+?)\.SH', data, re.DOTALL)
+    # re.findall will skip the later .SH tag and thus skip the later section.
+    # To fix this, '.SE' is used to mark the end of the section so the next
+    # '.SH' can be find by re.findall
+
+    page_type =  re.search(r'\n\.SH TYPE\n(.+?)\n', data)
+    if page_type and 'class' in page_type.group(1):
         # Member functions
         class_name = re.search(r'\n\.SH NAME\n(.+?) ', data).group(1)
 
-        for pat in ['MEMBER FUNCTIONS', 'PUBLIC MEMBERS', 'MEMBERS']:
-            try:
-                st = re.search(r'\n\.SH "%s"(.+?)\n\.SH' % pat,
-                                                data,re.DOTALL).group(1)
-                if st: break
-            except AttributeError:
-                if pat != 'MEMBERS': continue
-                raise Exception('pattern not match')
+        secs = re.findall(r'\n\.SH "(.+?)"(.+?)\.SE', data, re.DOTALL)
 
-        sts = re.sub(r'\n.IP "([^:]+)"', r'\n.IP "%s::\1"' % class_name, st)
-        # Replace (constructor) (destructor)
-        sts = re.sub(r'\(constructor\)', r'%s' % class_name, sts)
-        sts = re.sub(r'\(destructor\)', r'~%s' % class_name, sts)
-        index = data.index(st)
-        data = data[:index] + sts + data[index + len(st):]
-
-        # Inherited members tags
-        for pat in ['MEMBERS', 'MEMBER FUNCTIONS']:
-            # secs = re.findall(r'(\n\.SH "%s INHERITED FROM .+?".*?)\n\.SH'
-            #                  % pat, data, re.DOTALL)
-            #
-            # re only retern the first match, don't know why.
-            # Workaround: use string index instead
-            secs = []
-            index1 = 0
-            while True:
-                try:
-                    index1 = data.index('.SH "%s INHERITED FROM' % pat, index1)
-                    index2 = data.index('.SH', index1 + 3)
-                except ValueError:
-                    break
-                secs.append(data[index1: index2])
-                index1 = index2
-
-            for st in secs:
-                class_name = re.search(r'\.SH "%s INHERITED FROM (.+?)"'
-                                       % pat, st).group(1).lower()
-                sts = re.sub(r'\n\.IP "(.+)"', r'\n\.IP "%s::\1"' %
-                                                        class_name, st)
-                index = data.index(st)
-                data = data[:index] + sts + data[index + len(st):]
+        for sec, content in secs:
+            if 'MEMBER' in sec and 'INHERITED' not in sec and\
+               sec != 'MEMBER TYPES':
+                contents = re.sub(r'\n\.IP "([^:]+?)"', r'\n\.IP "%s::\1"'
+                                  % class_name, content)
+                # Replace (constructor) (destructor)
+                contents = re.sub(r'\(constructor\)', r'%s' % class_name,
+                                  contents)
+                contents = re.sub(r'\(destructor\)', r'~%s' % class_name,
+                                  contents)
+                data = data.replace(content, contents)
+            elif 'MEMBER' in sec and 'INHERITED' in sec:
+                inherit = re.search(r'.+?INHERITED FROM (.+)',
+                                    sec).group(1).lower()
+                contents = re.sub(r'\n\.IP "(.+)"', r'\n\.IP "%s::\1"'
+                                  % inherit, content)
+                data = data.replace(content, contents)
 
     # Remove invalid marking in tables
     for st in re.findall(r'\.TS(.+?)\.TE', data, re.DOTALL):
@@ -193,6 +182,9 @@ def cplusplus2groff(data):
         sts = re.sub(r'\n\.', r'\n\\.', sts)
         index = data.index(st)
         data = data[:index] + sts + data[index + len(st):]
+
+    # Remove pseudo macro .SE
+    data = data.replace('\n.SE', '')
 
     return data
 
@@ -234,7 +226,7 @@ def test():
     Simple Text
     '''
     #name = raw_input('What manual page do you want?')
-    name = 'auto_ptr'
+    name = 'streambuf'
     ifs = urllib.urlopen('http://www.cplusplus.com/' + name)
     print cplusplus2groff(ifs.read()),
 
