3915a155d07837452796e677b108d127845b75f7
AZ Huang <aitjcize@gmail.com>
2010-08-04 10:30:29 +0800

    Fix some dev related files

--- dev/build-deb.sh
+++ dev/build-deb.sh
@@ -13,14 +13,13 @@ tar -zcf manpages-cpp_$ver.orig.tar.gz manpages-cpp-$ver
 cd manpages-cpp-$ver
 dh_make -s -b -p manpages-cpp
 
-echo `pwd`
 cp dev/rules dev/control debian
 cd debian
 rm *.ex *.EX README.*
 dch -e
 cd ..
 
-if [ "$1" == "build" ]; then
+if [ "$1" == "deb" ]; then
   dpkg-buildpackage -rfakeroot
 elif [ "$1" == "ppa" ]; then
   debuild -S
