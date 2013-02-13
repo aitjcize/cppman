5c0d6586f6219512152f5e2ad32026d1abfad0dc
AZ Huang <aitjcize@gmail.com>
2013-02-13 11:53:37 +0800

    Add C++11 version tab to page name.

--- cppman/Formatter.py
+++ cppman/Formatter.py
@@ -58,6 +58,7 @@ rps = [
          r'\n.SE\n.SH TYPE\n\1\n'
          r'\n.SE\n.SH SYNOPSIS\n\3\n'
          r'\n.SE\n.SH DESCRIPTION\n' % datetime.date.today(), re.S),
+        (r'<span class="C_ico cpp11warning".*?>', r' [C++11]', re.S),
         # Remove empty #include
         (r'#include \n.sp\n', r'', 0),
         # Remove empty sections
