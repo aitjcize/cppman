462d4040ac113d015a7ca67aa4b6cd9302a2460e
AZ Huang <aitjcize@gmail.com>
2013-06-05 16:43:10 +0800

    Bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ _data_files = [
 
 setup(
 	name = 'cppman',
-	version = '0.2.3',
+	version = '0.2.4',
 	description = 'C++ man pages generater that generates C++ man pages'
                       'from cplusplus.com',
 	author = 'Wei-Ning Huang (AZ)',
