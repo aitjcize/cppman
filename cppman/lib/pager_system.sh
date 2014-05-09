22dfe86916950ea0c7f83074f661ebfe20c70661
AZ Huang <aitjcize@gmail.com>
2014-05-09 20:45:24 +0800

    Update year.

--- cppman/lib/pager_system.sh
+++ cppman/lib/pager_system.sh
@@ -2,7 +2,7 @@
 #
 # pager_system.sh
 #
-# Copyright (C) 2010 - 2013  Wei-Ning Huang (AZ) <aitjcize@gmail.com>
+# Copyright (C) 2010 - 2014  Wei-Ning Huang (AZ) <aitjcize@gmail.com>
 # All Rights reserved.
 #
 # This file is part of cppman.
