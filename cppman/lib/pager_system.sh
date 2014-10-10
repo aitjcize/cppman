f800902711f4dc947cfc63038e1cc47ef1338848
Wei-Ning Huang <aitjcize@gmail.com>
2014-10-10 11:12:22 -0700

    Use -Tascii to render table more nicely

--- cppman/lib/pager_system.sh
+++ cppman/lib/pager_system.sh
@@ -28,4 +28,4 @@
 
 [ -z "$PAGER" ] && PAGER=less
 
-cat "$1" | gunzip | groff -t -c -m man -Tascii -rLL=$2n -rLT=$2n 2> /dev/null | $PAGER
+zcat "$1" | groff -t -c -m man -Tutf8 -rLL=$2n -rLT=$2n 2> /dev/null | $PAGER
