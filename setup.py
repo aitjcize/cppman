2146415b7045a74145b454994702291649fe9bba
Wei-Ning Huang <aitjcize@gmail.com>
2015-10-08 14:31:27 -0700

    Bump version to 0.4.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ _data_files = [
 
 setup(
         name = 'cppman',
-        version = '0.4.5',
+        version = '0.4.6',
         description = 'C++ 98/11/14 manual pages for Linux/MacOS',
         author = 'Wei-Ning Huang (AZ)',
         author_email = 'aitjcize@gmail.com',
