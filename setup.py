169be2889ad2d88d133f3f66a53e397339a9200c
Wei-Ning Huang <w@cobinhood.com>
2018-08-18 19:53:25 +0800

    Bump version to 0.4.9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ _data_files = [
 
 setup(
         name = 'cppman',
-        version = '0.4.8',
+        version = '0.4.9',
         description = 'C++ 98/11/14 manual pages for Linux/MacOS',
         author = 'Wei-Ning Huang (AZ)',
         author_email = 'aitjcize@gmail.com',
