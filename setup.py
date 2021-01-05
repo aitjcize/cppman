6adda5761348d15c4237c76cd863cf7aae028423
Wei-Ning Huang <aitjcize@gmail.com>
2021-01-05 11:06:58 +0800

    Bump version to 0.5.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ _data_files = [
 
 setup(
         name = 'cppman',
-        version = '0.5.1',
+        version = '0.5.2',
         description = 'C++ 98/11/14/17/20 manual pages for Linux/MacOS',
         author = 'Wei-Ning Huang (AZ)',
         author_email = 'aitjcize@gmail.com',
